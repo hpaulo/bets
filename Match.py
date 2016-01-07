35fef68bfedffaa9e8d85dd22b5727064196c819
David del Río Medina <ddrm86@gmail.com>
2016-01-07 10:32:11 +0100

    feat: Add basic Match class

---
+++ Match.py
@@ -0,0 +1,7 @@
+class Match:
+    def __init__(self, local, visitor, mults, date):
+        self.local = local
+        self.visitor = visitor
+        self.mults = mults
+        self.date = date
+        self.result = []
