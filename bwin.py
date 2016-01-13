f0cfd1f7abad2fe079da1ccbc046eddd81a472e5
David del Río Medina <ddrm86@gmail.com>
2016-01-13 10:43:05 +0100

    feat: Scrap all pages

--- bwin.py
+++ bwin.py
@@ -1,24 +1,24 @@
 from bs4 import BeautifulSoup
-from datetime import datetime
+from datetime import datetime, date
 from Competition import Competition
 from Match import Match
 import requests
 
 
-def get_date(day_elem):
+def _get_date(day_elem):
     text = day_elem.text.strip()
     str_date = text[-10:]
     match_date = datetime.strptime(str_date, '%d/%m/%Y').date()
     return match_date
 
 
-def get_match_data(match_elem):
+def _get_match_data(match_elem):
     teams = [option.text for option in match_elem.find_all(class_='option-name') if option.text != 'X']
     mults = [float(odds.text) for odds in match_elem.find_all(class_='odds')]
     return teams, mults
 
 
-def get_competition(competition_elem):
+def _get_competition(competition_elem):
     code = competition_elem.div['data-league']
     name = competition_elem.a.text
     name = ' '.join([name.strip() for name in name.splitlines()])
@@ -26,15 +26,31 @@ def get_competition(competition_elem):
     return comp
 
 
-r = requests.get('https://sports.bwin.es/es/sports/4/apuestas/f%c3%batbol/2016-01-11')
-soup = BeautifulSoup(r.text, 'html.parser')
-competitions = soup.find_all(class_='event-group-level1')
-for competition in competitions:
-    comp_obj = get_competition(competition)
-    print(comp_obj)
-    match_listing = competition.parent.findNext('ul')
-    matches = match_listing.find_all(class_='col3 three-way')
-    for match in matches:
-        teams, mults = get_match_data(match)
-        match_obj = Match(teams[0], teams[1], mults, comp_obj)
-        print(match_obj)
+def get_matches(day):
+    finished = False
+    page = 0
+    url = 'https://sports.bwin.es/es/sports/indexmultileague'
+    post_data = {'sportId': '4', 'page': str(page), 'dateFilter': str(day)}
+
+    while not finished:
+        request = requests.post(url, post_data)
+        soup = BeautifulSoup(request.text, 'html.parser')
+        competitions = soup.find_all(class_='event-group-level1')
+        for competition in competitions:
+            comp_obj = _get_competition(competition)
+            print(comp_obj)
+            match_listing = competition.parent.findNext('ul')
+            matches = match_listing.find_all(class_='col3 three-way')
+            for match in matches:
+                teams, mults = _get_match_data(match)
+                match_obj = Match(teams[0], teams[1], mults, comp_obj)
+                print(match_obj)
+        if len(competitions) == 0:
+            finished = True
+        page += 1
+        post_data['page'] = page
+
+
+if __name__ == "__main__":
+    today = date.today()
+    get_matches(today)
