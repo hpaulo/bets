35822e085580cacce4b2133946c92033ee1ac7ca
David del Río Medina <ddrm86@gmail.com>
2016-01-15 14:06:00 +0100

    feat: Add raw example parsing of first 6 pages

--- results.py
+++ results.py
@@ -1,6 +1,22 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 import requests
+import locale
 
-r = requests.get('https://sports.bwin.es/es/sports/results?sport=4&region=28&league=16109&period=ThreeDays')
-soup = BeautifulSoup(r.text, 'html.parser')
-print(soup.prettify())
+locale.setlocale(locale.LC_ALL, 'Spanish')
+
+enlace = 'https://sports.bwin.es/es/sports/results?sport=4&period=ThreeDays&sort=Date&page='
+
+for i in range(6):
+    r = requests.get(enlace + str(i))
+    soup = BeautifulSoup(r.text, 'html.parser')
+    for date in soup.find_all(class_='result-group date'):
+        print(datetime.strptime(date.h3.text.strip(), '%A, %d de %B de %Y').date())
+        competitions = soup.find_all(class_='result-subgroup league')
+        for competition in competitions:
+            print(competition.thead.tr.th.text.strip())
+            matches = competition.tbody.find_all('tr')
+            for match in matches:
+                name = match.find(class_='name')
+                result = match.find(class_='result')
+                print(name.text, result.text)
