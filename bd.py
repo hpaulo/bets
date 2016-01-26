e74e13cfc6838c0d2faa7e302185c063a7051561
David del Río Medina <ddrm86@gmail.com>
2016-01-26 11:40:49 +0100

    feat: Add logging

--- bd.py
+++ bd.py
@@ -1,5 +1,8 @@
 from pymongo import MongoClient
 from Match import Match
+import logging
+
+logger = logging.getLogger(__name__)
 
 client = MongoClient()
 db = client.bets
@@ -7,22 +10,28 @@ coll = db.matches
 
 
 def find_match(mdatetime, local, visitor):
+    logger.debug('Start finding match: %s, %s, %s', mdatetime, local, visitor)
     query = {'mdatetime': mdatetime, 'local': local, 'visitor': visitor}
     match_dict = coll.find_one(query)
     match = Match.from_json(match_dict)
+    logger.debug('Stop finding match: %s', match)
     return match
 
 
 def insert_new_matches(matches):
+    logger.debug('Start inserting new matches')
     for match_dict in matches:
         match = find_match(match_dict['mdatetime'], match_dict['local'], match_dict['visitor'])
         if match:
-            print('Found: ', match)
+            logger.debug('Match already in database')
         else:
-            print('NOT FOUND: ', match_dict)
+            logger.debug('New match: %s', match_dict)
             coll.insert_one(match_dict)
+    logger.debug('Finish inserting new matches', len(matches))
 
 
 def update_results(match, result):
+    logger.debug('Start updating match results: %s, %s', match, result)
     coll.update_one({'mdatetime': str(match.mdatetime), 'local': match.local, 'visitor': match.visitor},
                     {'$set': {'result': result}})
+    logger.debug('Finish updating match results')
